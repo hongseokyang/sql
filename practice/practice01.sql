b4303e755ea1410f16237d13a96c3f74c57ef95e
bit <bit@DESKTOP-S8HQR48>
2019-09-03 15:49:12 +0900

    commit

--- practice/practice01.sql
+++ practice/practice01.sql
@@ -2,3 +2,9 @@
 
 -- 문제1
 -- 사번이 10944인 사원의 이름은(전체 이름)
+select concat(first_name," ",last_name) name
+from employees
+where emp_no = 10944;
+
+select concat(first_name," ",last_name) "이름"
+from employees;
